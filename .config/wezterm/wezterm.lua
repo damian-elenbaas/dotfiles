ded3b24f026ec39b56c7e98f661eee68908dc132
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-02-08 15:06:33 +0100

    Init

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Kanagawa (Gogh)'
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font_size = 13.7
+config.window_background_opacity = 0.6
+
+-- and finally, return the configuration to wezterm
+return config
